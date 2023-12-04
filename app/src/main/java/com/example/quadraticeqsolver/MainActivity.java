e766e8d56b5be3aa24a5641eba24941d1e7d8627
NitinSigilipelli <sigilipellinitin@gmail.com>
2023-12-04 22:13:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quadraticeqsolver/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.quadraticeqsolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.quadraticeqsolver.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mainBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        MyEquation equation = new MyEquation(mainBinding);
+        mainBinding.setMyequation(equation);
+    }
+}
